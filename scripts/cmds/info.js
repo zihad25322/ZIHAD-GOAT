ccc45bf7f95e94dba191eaca511903decac09df2
𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬 <134224832+MD-JUBAED-AHMED-JOY@users.noreply.github.com>
2024-10-16 12:17:51 +0600

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -36,7 +36,7 @@ module.exports = {
 
       const img = `https://graph.facebook.com/100065050393649/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
 
-      const msg = `╔╝❮${name}❯╚╗\n━━━━━━━━━━━━━━━━━━━━━━\n𝗡𝗔𝗠𝗘: 𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬\n𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍: 𝐈𝐒𝐋𝐀𝐌\n𝐀𝐃𝐃𝐑𝐄𝐒𝐒: 𝗧𝗔𝗥𝗔𝗞𝗔𝗡𝗗𝗜, 𝗦𝗔𝗥𝗜𝗦𝗛𝗔𝗕𝗔𝗥𝗜, 𝗝𝗔𝗠𝗔𝗟𝗣𝗨𝗥\n𝐆𝐄𝐍𝐃𝐄𝐑: 𝐌𝐀𝐋𝐄\n𝐀𝐆𝐄: 16\n𝐑𝐄𝐋𝐀𝐓𝐈𝐎𝐍𝐒𝐇𝐈𝐏: 𝗦𝗜𝗡𝗚𝗟𝗘\n𝐖𝐎𝐑𝐊: 𝗦𝗧𝗨𝗗𝗘𝗡𝗧\n𝐆𝐌𝐀𝐈𝐋: 𝗷𝗼𝘆𝗮𝗵𝗺𝗲𝗱𝘁𝘁@𝗴𝗺𝗮𝗶𝗹.𝗰𝗼𝗺\n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊: https://www.facebook.com/mdjubaedahmedjoy?mibextid=ZbWKwL \n𝐌𝐀𝐒𝐒𝐄𝐍𝐆𝐄𝐑: m.me/100065050393649\n𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏: wa.me/+8801709045888\n𝐈𝐌𝐎: 01709045888\n𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌: @mdjubaedahmed\n━━━━━━━━━━━━━━━━━━━━━━\n\nBot Prefix: ( . )\nBot Name: 𝗝𝗢𝗬-𝗕𝗢𝗧\ngc Name: ${threadName}\nTime:【 ${date} || ${time} \n━━━━━━━━━━━━━━━━━━━━━━`;
+      const msg = `╔╝❮𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐄𝐃 𝐉𝐎𝐘❯╚╗\n━━━━━━━━━━━━━━━━━━━━━━\n𝗡𝗔𝗠𝗘: 𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬\n𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍: 𝐈𝐒𝐋𝐀𝐌\n𝐀𝐃𝐃𝐑𝐄𝐒𝐒: 𝗧𝗔𝗥𝗔𝗞𝗔𝗡𝗗𝗜, 𝗦𝗔𝗥𝗜𝗦𝗛𝗔𝗕𝗔𝗥𝗜, 𝗝𝗔𝗠𝗔𝗟𝗣𝗨𝗥\n𝐆𝐄𝐍𝐃𝐄𝐑: 𝐌𝐀𝐋𝐄\n𝐀𝐆𝐄: 16\n𝐑𝐄𝐋𝐀𝐓𝐈𝐎𝐍𝐒𝐇𝐈𝐏: 𝗦𝗜𝗡𝗚𝗟𝗘\n𝐖𝐎𝐑𝐊: 𝗦𝗧𝗨𝗗𝗘𝗡𝗧\n𝐆𝐌𝐀𝐈𝐋: 𝗷𝗼𝘆𝗮𝗵𝗺𝗲𝗱𝘁𝘁@𝗴𝗺𝗮𝗶𝗹.𝗰𝗼𝗺\n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊: https://www.facebook.com/mdjubaedahmedjoy?mibextid=ZbWKwL \n𝐌𝐀𝐒𝐒𝐄𝐍𝐆𝐄𝐑: m.me/100065050393649\n𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏: wa.me/+8801709045888\n𝐈𝐌𝐎: 01709045888\n𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌: @mdjubaedahmed\n━━━━━━━━━━━━━━━━━━━━━━\n\nBot Prefix: ( . )\nBot Name: 𝗝𝗢𝗬-𝗕𝗢𝗧\ngc Name: ${threadName}\nTime:【 ${date} || ${time} \n━━━━━━━━━━━━━━━━━━━━━━`;
 
       message.reply({
         body: msg,
