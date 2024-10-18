eb9a305b1bb969f24413576eecf0e66f2b9829bb
MR BAYJID FM'Z GUTI BAZ <zaraahmed2267@gmail.com>
2024-10-18 17:59:30 +0600

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -23,6 +23,6 @@ module.exports = {
     const hours = Math.floor((uptime / (60 * 60)) % 24);
     const days = Math.floor(uptime / (60 * 60 * 24));
     const uptimeString = `${hours} hours ${minutes} minutes ${seconds} second`;
-    api.sendMessage(`Hello Master, the bot has been running for ${uptimeString}.`, event.threadID);
+    api.sendMessage(`Hello Master, I AM BAYJID BOT the bot has been running for ${uptimeString}.`, event.threadID);
   }
 };
