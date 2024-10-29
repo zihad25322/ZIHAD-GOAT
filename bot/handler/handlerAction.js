68245aa02da7de9940f88afbbe0efcd38eec9e94
MR-SAGOR-Cyber-BD <130752960+MR-SAGOR-Cyber-BD@users.noreply.github.com>
2024-10-29 21:27:11 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,7 +72,7 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "💋"){
+        if(event.reaction == "🙏"){
   if(event.userID == "100065506668822"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
@@ -82,7 +82,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "❌"){
+        if(event.reaction == "🤬"){
   if(event.senderID == api.getCurrentUserID()){if(event.userID == "100065506668822"){
     message.unsend(event.messageID)
 }else{
