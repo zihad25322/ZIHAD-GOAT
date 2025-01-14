af87ed9466ce2af181ba16f0416d01066a635b00
zihad25322 <162394310+zihad25322@users.noreply.github.com>
2025-01-15 01:38:09 +0600

    Update and rename bayjid.js to Zihad.js

---
+++ scripts/cmds/Zihad.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+	 name: "zihad",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "bayjid") {
+ return message.reply({
+ body: " 「❥︎----ღ᭄_ᴬˢˢᴬᴸᴬᴹᴼᴸᴬᴵᴷᵁᴹ ..\n❥︎----ღ᭄_  ᴮᴿᴼᵀᴴᴱᴿ❞࿐.🌴.\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝐌𝐎𝐇𝐀𝐌𝐌𝐀𝐃 𝐙𝐈𝐇𝐀𝐃」",
+ attachment: await global.utils.getStreamFromURL("")
+ });
+ }
+ }
+}
