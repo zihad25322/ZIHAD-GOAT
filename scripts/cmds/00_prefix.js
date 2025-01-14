b04db54d3c3438f145edef76fc57f30dc9aa5a2c
zihad25322 <162394310+zihad25322@users.noreply.github.com>
2025-01-15 02:10:29 +0600

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -20,8 +20,8 @@ Yo, my prefix is [ 𓆩 _<?>_ 𓆪 ]\n
 ➥ &help [number of page] -> see commands
 ➥ &sim [message] -> talk to bot
 ➥ &callad [message] -> report any problem encountered
-➥ &help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/BAYJID.500k`,
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/I8JSUWI.jpeg")
+➥ &help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/xxn.zihad`,
+ attachment: await global.utils.getStreamFromURL("")
  });
  }
  }
